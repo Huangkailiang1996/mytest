ae79a03a59dd945866f61b6eae90145dd030de06
huangkailiang <2048519916@qq.com>
2023-07-03 15:24:21 +0800

    idea2022_git

---
+++ src/main/java/com/huang/mytest/controller/TestController.java
@@ -0,0 +1,15 @@
+package com.huang.mytest.controller;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+public class TestController {
+    @GetMapping("/a")
+    public void test() {
+        log.info("11111");
+    }
+}
